b6a1b4f260d7963399f14020f31d6cad33c1ae3c
Guilherme <guilherme.pomp@gmail.com>
2020-06-14 11:31:47 -0300

    Consumindo dados da API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './home.css';
 
-class Home extends Component{
+class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            filmes: []
+        }
+
+        this.loadFilms = this.loadFilms.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadFilms();
+    }
+
+    loadFilms() {
+        let url = 'http://sujeitoprogramador.com/r-api/?api=filmes';
+        fetch(url)
+            .then((r) => r.json())
+            .then((json) => {
+                this.setState({ filmes: json });
+                console.log(json);
+            })
+    }
+
     render() {
-        return(
-            <h1>Home</h1>
+        return (
+            <div className="container">
+                <div className="lista-filmes">
+                    {this.state.filmes.map((filme) => {
+                        return (
+                            <article key={filme.id}>
+                                <strong>{filme.nome}</strong>
+                                <img src={filme.foto} />
+                                <Link to="/">Acessar</Link>
+                            </article>
+                        )
+                    })}
+                </div>
+            </div>
         );
     }
 }
